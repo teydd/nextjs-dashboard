64c421290e2b1c17bcd4c9812efeb2e74eeb577e
teydd <chemosted@gmail.com>
2026-03-24 18:44:13 +0300

    update

--- auth.ts
+++ auth.ts
@@ -6,8 +6,7 @@ import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
  
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' }); 
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
